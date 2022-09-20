45df4f08827fe9500136d1b354cd1914d0e17c62
Renato Lara <larasantos22@hotmail.com>
2022-09-20 07:54:28 -0300

    Controller healthCheck

---
+++ src/main/java/br/com/fiap/abdctechapi/AbdctechapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.abdctechapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbdctechapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbdctechapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/abdctechapi/AbdctechapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.abdctechapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbdctechapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
